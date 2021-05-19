2a72a21acc8e66d9a3c2a80f5f96dfca02d249e3
Eftekar raghib <mdraghib17@gmail.com>
2021-05-19 22:40:37 +0600

    Category_sidebar fixced

--- Components/CategorySidebar/Categories.js
+++ Components/CategorySidebar/Categories.js
@@ -1,12 +1,30 @@
+import Link from 'next/link';
 import Styles from '../../styles/Home.module.css';
 const Categories = (props) => {
   return (
-    <div className={Styles.categories}>
-      <div className="">
-        <h1>Categories goes here</h1>
-      </div>
-      <div className="" onClick={() => props.setSidebar(!props.sidebar)}>
-        backdrop
+    <div className="">
+      <div className={`${Styles.categories} ${props.sidebar && 'd-block'}`}>
+        <div className={Styles.categories_list}>
+          <div
+            className={Styles.cross}
+            onClick={() => props.setSidebar(!props.sidebar)}>
+            x
+          </div>
+          <ul>
+            {Array(55)
+              .fill()
+              .map((route) => (
+                <li>
+                  <Link href="">
+                    <a>cat1</a>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div
+          className={Styles.categories_backdrop}
+          onClick={() => props.setSidebar(!props.sidebar)}></div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,40 @@
 import Header from '../PrimaryComponents/Header/Header';
 import Meta from '../PrimaryComponents/Utility/Meta';
-import styles from '../styles/Home.module.css';
+import Styles from '../styles/Home.module.css';
 import dynamic from 'next/dynamic';
 import Features from '../Components/Features/Features';
 import Categories from '../Components/CategorySidebar/Categories';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [categorySidebar, setCategorySidebar] = useState(false);
+  useEffect(() => {
+    document.body.style.overflow = categorySidebar ? 'hidden' : '';
+  }, [categorySidebar]);
   // prevent owlCarrousel ServerSide rendering
   const HomeBanner = dynamic(import('../Components/Banner/HomeBanner'), {
     ssr: false,
   });
   return (
-    <div className={''}>
+    <div className={Styles.home_wrapper}>
       <Meta />
       <Header sidebar={categorySidebar} setSidebar={setCategorySidebar} />
-      {categorySidebar && (
-        <Categories sidebar={categorySidebar} setSidebar={setCategorySidebar} />
-      )}
+      <Categories sidebar={categorySidebar} setSidebar={setCategorySidebar} />
       <HomeBanner />
       <Features />
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <Features />
+      <main className={Styles.main}>
+        <h1 className={Styles.title}>
           Welcome to <a href="https://uparzon.com.bd">Uparzon</a>
         </h1>
       </main>
