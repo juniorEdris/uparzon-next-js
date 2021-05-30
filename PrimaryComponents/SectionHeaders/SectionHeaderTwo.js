ba61aebe42ef7f1dcdedb5eaf2af4a9f03fd52fc
eftekarRaghib <mdraghib17@gmail.com>
2021-05-30 23:54:29 +0600

    littile fixed

--- PrimaryComponents/SectionHeaders/SectionHeaderTwo.js
+++ PrimaryComponents/SectionHeaders/SectionHeaderTwo.js
@@ -1,7 +1,7 @@
 const SectionHeaderTwo = (props) => {
     return (
         <div className="section_header_two chilldren-gap-bottom">
-            <h5 className={`${props.list && 'font-size-1rem'} title`}>{ props.section_title }</h5>
+            <h5 className={`${props.list && 'font-size-point7rem'} title`}>{ props.section_title }</h5>
         </div>
      );
 }
